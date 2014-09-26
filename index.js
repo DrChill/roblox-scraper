f4a565b86b70a0608f54f654bc44372214d5ed14
DrChill <DrChill@users.noreply.github.com>
2014-09-26 04:20:34 -0500

    add beginning of the game class

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 var User = require('./lib/user.js');
 var AssetType = require('./lib/asset-type.js');
+var Game = require('./lib/game.js');
 
 exports.getUser = function(nameOrId) {
 	return new User(nameOrId);
 };
+
+exports.getGame = function(id) {
+	return new Game(id);
+}
 
 exports.AssetType = AssetType;
